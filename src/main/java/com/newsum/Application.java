c7ec56bcf4582fc78a6e0daa7505792e7a785168
Micah Newsum <mnewsum@glenair.com>
2021-03-07 14:52:51 -0800

    Update Application.java

--- src/main/java/com/newsum/Application.java
+++ src/main/java/com/newsum/Application.java
@@ -7,6 +7,10 @@ import java.util.List;
 
 public class Application {
   public static void main(String[] args) {
+    arrayListClient();
+  }
+
+  private static void arrayListClient(){
     List<Employee> employees = new ArrayList<>();
 
     employees.add(new Employee("Micah","Newsum",1));
@@ -25,6 +29,14 @@ public class Application {
     // inserting element at index is O(n) because may require shift of every element in backing array, as is the case below
     System.out.println("Inserting elements");
     employees.add(0,new Employee("Micah","Newsum",1));
+    employees.forEach(System.out::println);
+
+    System.out.println("Check for element existence");
+    System.out.println(employees.contains(new Employee("Micah", "Newsum", 1)));
+    System.out.println(employees.indexOf(new Employee("Aysha", "Newsum",2)));
+
+    // removing an element is O(n) because the operation may require every element in the backing array to be shifted down an index
+    employees.remove(0);
     employees.forEach(System.out::println);
   }
 }
