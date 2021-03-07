c730d1a84928e4e5ed6af1672f64ae9431a5c6ca
Micah Newsum <mnewsum@glenair.com>
2021-03-07 13:56:54 -0800

    Override hashCode & toString methods.

--- src/main/java/com/newsum/model/Employee.java
+++ src/main/java/com/newsum/model/Employee.java
@@ -12,7 +12,35 @@ public class Employee {
   }
 
   @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+
+    Employee employee = (Employee) o;
+
+    if (id != employee.id) {
+      return false;
+    }
+    if (!firstName.equals(employee.firstName)) {
+      return false;
+    }
+    return lastName.equals(employee.lastName);
+  }
+
+  @Override
+  public int hashCode() {
+    int result = firstName.hashCode();
+    result = 31 * result + lastName.hashCode();
+    result = 31 * result + id;
+    return result;
+  }
+
+  @Override
   public String toString() {
-    return String.format("{firstName='%s', lastName='%s', id=%d}",firstName,lastName,id);
+    return String.format("Employee{firstName='%s', lastName='%s', id=%d}",firstName,lastName,id);
   }
 }
